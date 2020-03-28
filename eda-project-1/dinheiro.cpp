655767b37cd439557a54afb858afe6ec366b3689
pedro-uma <61146730+pedro-uma@users.noreply.github.com>
2020-03-28 20:40:52 +0000

    Add files via upload
    Vejam se falta alguma que devia acrescentar :)

---
+++ eda-project-1/dinheiro.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<stdlib.h>
+#include<time.h>
+#include "dinheiro.h"
+using namespace std;
+
+
+
+void fundos() {
+	
+	int moedasdoiseuros = rand() % 11 + 10; //de 10 a 20
+	
+	int moedasumeuros = rand() % 11 + 10; //de 10 a 20
+	//int moedasumeuros = 2;
+
+	int moedascinquentacents = rand() % 11 + 10; //de 10 a 20
+	
+	int moedasvintecents = rand() % 11 + 10; //de 10 a 20
+	
+	int moedasdezcents = rand() % 11 + 10; //de 10 a 20
+	
+	int moedascincocents = rand() % 11 + 10; //de 10 a 20
+	
+	/*cout << "2 ->" << moedasdoiseuros<<endl;
+	cout << "1-> " << moedasumeuros << endl;
+	cout << "0.5 ->" << moedascinquentacents << endl;
+	cout << "0.2-> " << moedasvintecents << endl;
+	cout << "0.1-> " << moedasdezcents << endl;
+	cout << "0.05-> " << moedascincocents << endl;*/ //Para teste!!!!
+
+	if (moedasdoiseuros <= 3 || moedasumeuros <= 3 || moedascinquentacents <= 3 || moedasvintecents <= 3 || moedasdezcents <= 3 || moedascincocents <= 3) { //Este if para dizer apenas uma vez o ***AVISO***
+		cout << "***AVISO***" << endl;
+		if (moedasdoiseuros <= 3) {
+			cout << "Existem apenas " << moedasdoiseuros << " moedas de dois euros"<<endl;
+		}
+		else if (moedasumeuros <= 3) {
+			cout << "Existem apenas " << moedasumeuros << " moedas de dois euros"<<endl;
+		}
+		else if (moedascinquentacents <= 3) {
+			cout << "Existem apenas " << moedascinquentacents << " moedas de dois euros"<<endl;
+		}
+		else if (moedasvintecents <=3) {
+			cout << "Existem apenas " << moedasvintecents << " moedas de dois euros"<<endl;
+		}
+		else if (moedasdezcents <=3) {
+			cout << "Existem apenas " << moedasdezcents << " moedas de dois euros"<<endl;
+		}
+		else if (moedascincocents <=3) {
+			cout << "Existem apenas " << moedascincocents << " moedas de dois euros" << endl;
+		}
+
+
+	}
+
+}
+
+
+
