28a12f649ceac49b229a219150b6fc41a29da389
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-04-13 16:55:56 +0100

    Update funcionario.h

--- eda-project-1/funcionario.h
+++ eda-project-1/funcionario.h
@@ -7,3 +7,10 @@ void menu_funcionario(slot* maquina, int moedas[6], int numSlots);
 void imprimir_produtos(slot* maquina, int numSlots);
 
 void alterar_preco(slot* maquina, int numSlots);
+
+void gravarMaquinaSlots(slot* maquina, int numSlots, string fileName);
+void gravarMaquinanumSlots(slot* maquina, int numSlots, string fileName);
+void gravarMaquinaMoedas(slot* maquina, string fileName, int moedas[]);
+
+void carregarMaquinanumSlots(slot* maquina, int numSlots, string fileName);
+void carregarMaquinaMoedas(slot* maquina, string fileName, int moedas[]);
