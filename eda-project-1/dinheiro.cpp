6b6df3f79a39a999d3e263badaf970fa6a89890c
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-04-15 15:10:36 +0100

    Atualizar os fundos (poder ver fundos ANTES de carregar ou remover moedas

--- eda-project-1/dinheiro.cpp
+++ eda-project-1/dinheiro.cpp
@@ -39,72 +39,87 @@ void verificarFundos(int moedas[6])
 			cout << moedas[1] << " moedas de 1 euros." << endl;
 		}
 		if (moedas[2] <= 3) {
-			cout << moedas[2] << " moedas de 50 cêntimos." << endl;
+			cout << moedas[2] << " moedas de 50 cÃªntimos." << endl;
 		}
 		if (moedas[3] <= 3) {
-			cout << moedas[3] << " moedas de 20 cêntimos." << endl;
+			cout << moedas[3] << " moedas de 20 cÃªntimos." << endl;
 		}
 		if (moedas[4] <= 3) {
-			cout << moedas[4] << " moedas de 10 cêntimos." << endl;
+			cout << moedas[4] << " moedas de 10 cÃªntimos." << endl;
 		}
 		if (moedas[5] <= 3) {
-			cout << moedas[5] << " moedas de 5 cêntimos." << endl;
+			cout << moedas[5] << " moedas de 5 cÃªntimos." << endl;
 		}
 	}
 }
 
-	void remover_fundos(int moedas[6])
-	{
-		int remover;
-		cout << "***Remover Fundos***"<<endl;
-		for (int i = 0; i < 6; i++) {
-			cout << "Quantas moedas de ";
-			switch (i) {
-			case 0:
-				cout << "2 euros";
-				break;
-			case 1:
-				cout << "1 euro";
-				break;
-			case 2:
-				cout << "50 cêntimos";
-				break;
-			case 3:
-				cout << "20 cêntimos";
-				break;
-			case 4:
-				cout << "10 cêntimos";
-				break;
-			case 5:
-				cout << "5 cêntimos";
-				break;
-			default:
-				break;
-			}
-			cout << " deseja remover?"<< endl;
-			cin >> remover;
-			
-			while(moedas[i] - remover < 0)
-			{
-				cout << "O numero de moedas sera negativo/inválido! Por favor digite outra vez o numero de moedas que pretende retirar: ";
-				cin >> remover;
-			}
-			moedas[i] = moedas[i] - remover;
+void remover_fundos(int moedas[6])
+{
+	//TEMOS QUE VER ANTES O FUNDOS PARA PODER REMOVER
+	cout << "*** Fundos ***" << endl;
+	cout << "2 euros ->" << moedas[0] << endl;
+	cout << "1 euros-> " << moedas[1] << endl;
+	cout << "50 cÃªntimos->" << moedas[2] << endl;
+	cout << "20 cÃªntimos-> " << moedas[3] << endl;
+	cout << "10 cÃªntimos -> " << moedas[4] << endl;
+	cout << "5 cÃªntimos-> " << moedas[5] << endl;
+	cout << endl;
+	int remover;
+	cout << "***Remover Fundos***"<<endl;
+	for (int i = 0; i < 6; i++) {
+		cout << "Quantas moedas de ";
+		switch (i) {
+		case 0:
+			cout << "2 euros";
+			break;
+		case 1:
+			cout << "1 euro";
+			break;
+		case 2:
+			cout << "50 cÃªntimos";
+			break;
+		case 3:
+			cout << "20 cÃªntimos";
+			break;
+		case 4:
+			cout << "10 cÃªntimos";
+			break;
+		case 5:
+			cout << "5 cÃªntimos";
+			break;
+		default:
+			break;
 		}
-
-		cout << "*** Fundos ***" << endl;
-		cout << "2 euros ->" << moedas[0] << endl;
-		cout << "1 euros-> " << moedas[1] << endl;
-		cout << "50 cêntimos->" << moedas[2] << endl;
-		cout << "20 cêntimos-> " << moedas[3] << endl;
-		cout << "10 cêntimos -> " << moedas[4] << endl;
-		cout << "5 cêntimos-> " << moedas[5] << endl;
-		cout << endl;
-
+		cout << " deseja remover?"<< endl;
+		cin >> remover;
+		while(moedas[i] - remover < 0)
+		{
+			cout << "O numero de moedas sera negativo/invÃ¡lido! Por favor digite outra vez o numero de moedas que pretende retirar: ";
+			cin >> remover;
+		}
+		moedas[i] = moedas[i] - remover;
 	}
+	cout << "*** Fundos ***" << endl;
+	cout << "2 euros ->" << moedas[0] << endl;
+	cout << "1 euros-> " << moedas[1] << endl;
+	cout << "50 cÃªntimos->" << moedas[2] << endl;
+	cout << "20 cÃªntimos-> " << moedas[3] << endl;
+	cout << "10 cÃªntimos -> " << moedas[4] << endl;
+	cout << "5 cÃªntimos-> " << moedas[5] << endl;
+	cout << endl;
+}
 
 
 	void carregar_moedas(int moedas[6]) {
+		//TEMOS QUE VER ANTES O FUNDOS PARA PODER CARREGAR
+		cout << "*** Fundos ***" << endl;
+		cout << "2 euros ->" << moedas[0] << endl;
+		cout << "1 euros-> " << moedas[1] << endl;
+		cout << "50 cÃªntimos->" << moedas[2] << endl;
+		cout << "20 cÃªntimos-> " << moedas[3] << endl;
+		cout << "10 cÃªntimos -> " << moedas[4] << endl;
+		cout << "5 cÃªntimos-> " << moedas[5] << endl;
+		cout << endl;
 		int adicionar = -1;
 		cout << "*** Carregar moedas ***" << endl;
 		for (int j = 0; j < 6; j++) {
@@ -119,31 +134,38 @@ void verificarFundos(int moedas[6])
 				moedas[1] = moedas[1] + adicionar;
 			}
 			else if (j == 2) {
-				cout << "Quantas moedas de 50 cêntimos deseja adicionar: " << endl;
+				cout << "Quantas moedas de 50 cÃªntimos deseja adicionar: " << endl;
 				cin >> adicionar;
 				moedas[2] = moedas[2] + adicionar;
 			}
 			else if (j == 3) {
-				cout << "Quantas moedas de 20 cêntimos deseja adicionar: " << endl;
+				cout << "Quantas moedas de 20 cÃªntimos deseja adicionar: " << endl;
 				cin >> adicionar;
 				moedas[3] = moedas[3] + adicionar;
 			}
 			else if (j == 4) {
-				cout << "Quantas moedas de 10 cêntimos deseja adicionar: " << endl;
+				cout << "Quantas moedas de 10 cÃªntimos deseja adicionar: " << endl;
 				cin >> adicionar;
 				moedas[4] = moedas[4] + adicionar;
 			}
 			else if (j == 5) {
-				cout << "Quantas moedas de 5 cêntimos deseja adicionar: " << endl;
+				cout << "Quantas moedas de 5 cÃªntimos deseja adicionar: " << endl;
 				cin >> adicionar;
 				moedas[5] = moedas[5] + adicionar;
 			}
 			// verificar se o valor inserido foi positivo
 			if (adicionar < 0) {
-				cout << "Insira um valor positivo valor positivo!: " << endl;
+				cout << "Insira um valor positivo!: " << endl;
 				j--;
 			}
 		}
 		cout << "** Moedas carregadas com sucesso **";
-		verificarFundos(moedas);
+		cout << "*** Fundos ***" << endl;
+		cout << "2 euros ->" << moedas[0] << endl;
+		cout << "1 euros-> " << moedas[1] << endl;
+		cout << "50 cÃªntimos->" << moedas[2] << endl;
+		cout << "20 cÃªntimos-> " << moedas[3] << endl;
+		cout << "10 cÃªntimos -> " << moedas[4] << endl;
+		cout << "5 cÃªntimos-> " << moedas[5] << endl;
+		cout << endl;
 	}
