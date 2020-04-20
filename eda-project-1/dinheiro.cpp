533b85c2dfdde7efd3f0bef54d4e2eac568e5976
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-04-20 16:22:29 +0100

    Dar uns retoques nos couts

--- eda-project-1/dinheiro.cpp
+++ eda-project-1/dinheiro.cpp
@@ -79,12 +79,12 @@ void remover_fundos(int moedas[6])
 		cin >> remover;
 		while(moedas[i] - remover < 0)
 		{
-			cout << "O numero de moedas sera negativo/inválido! Por favor digite outra vez o numero de moedas que pretende retirar: ";
+			cout << "O número de moedas será negativo/inválido! Por favor digite outra vez o número de moedas que pretende retirar: ";
 			cin >> remover;
 		}
 		moedas[i] = moedas[i] - remover;
 	}
-	cout << "** Fundos removidos com sucesso **";
+	cout << "** Fundos removidos com sucesso! **";
 	imprimeFundos(moedas);
 }
 
@@ -96,32 +96,32 @@ void carregar_moedas(int moedas[6]) {
 	cout << "*** Carregar moedas ***" << endl;
 	for (int j = 0; j < 6; j++) {
 		if (j == 0) {
-			cout << "Quantas moedas de 2 euros deseja adicionar: " << endl;
+			cout << "Quantas moedas de 2 euros deseja adicionar? " << endl;
 			cin >> adicionar;
 			moedas[0] = moedas[0] + adicionar;
 		}
 		else if (j == 1) {
-			cout << "Quantas moedas de 1 euro deseja adicionar: " << endl;
+			cout << "Quantas moedas de 1 euro deseja adicionar? " << endl;
 			cin >> adicionar;
 			moedas[1] = moedas[1] + adicionar;
 		}
 		else if (j == 2) {
-			cout << "Quantas moedas de 50 cêntimos deseja adicionar: " << endl;
+			cout << "Quantas moedas de 50 cêntimos deseja adicionar? " << endl;
 			cin >> adicionar;
 			moedas[2] = moedas[2] + adicionar;
 		}
 		else if (j == 3) {
-			cout << "Quantas moedas de 20 cêntimos deseja adicionar: " << endl;
+			cout << "Quantas moedas de 20 cêntimos deseja adicionar? " << endl;
 			cin >> adicionar;
 			moedas[3] = moedas[3] + adicionar;
 		}
 		else if (j == 4) {
-			cout << "Quantas moedas de 10 cêntimos deseja adicionar: " << endl;
+			cout << "Quantas moedas de 10 cêntimos deseja adicionar? " << endl;
 			cin >> adicionar;
 			moedas[4] = moedas[4] + adicionar;
 		}
 		else if (j == 5) {
-			cout << "Quantas moedas de 5 cêntimos deseja adicionar: " << endl;
+			cout << "Quantas moedas de 5 cêntimos deseja adicionar? " << endl;
 			cin >> adicionar;
 			moedas[5] = moedas[5] + adicionar;
 		}
@@ -131,7 +131,7 @@ void carregar_moedas(int moedas[6]) {
 			j--;
 		}
 	}
-	cout << "** Moedas carregadas com sucesso **";
+	cout << "** Moedas carregadas com sucesso! **";
 	imprimeFundos(moedas);
 }
 
@@ -145,10 +145,10 @@ bool VerificarAviso(int moedas[6]) {
 void imprimeFundos(int moedas[6])
 {
 	cout << "*** Fundos ***" << endl;
-	cout << "2 euros ->" << moedas[0] << endl;
-	cout << "1 euros-> " << moedas[1] << endl;
-	cout << "50 centimos->" << moedas[2] << endl;
-	cout << "20 centimos-> " << moedas[3] << endl;
-	cout << "10 centimos -> " << moedas[4] << endl;
-	cout << "5 centimos-> " << moedas[5] << endl << endl;
+	cout << "2 euros -> " << moedas[0] << endl;
+	cout << "1 euros -> " << moedas[1] << endl;
+	cout << "50 cêntimos -> " << moedas[2] << endl;
+	cout << "20 cêntimos -> " << moedas[3] << endl;
+	cout << "10 cêntimos -> " << moedas[4] << endl;
+	cout << "5 cêntimos -> " << moedas[5] << endl << endl;
 }
